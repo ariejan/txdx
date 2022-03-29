47f2e88c1ed43155c355fb29ce73c5ce79b3d8ea
Ariejan de Vroom <ariejan@devroom.io>
2022-03-29 14:57:55 +0200

    Automatically add due,project or context to new item when a filter is selected

--- lib/settings.dart
+++ lib/settings.dart
@@ -4,6 +4,7 @@ const settingsFileAutoReload = 'file_autoreload';
 const settingsThemeUseSystem = 'theme_system_brightness';
 const settingsThemeUseDark = 'theme_use_dark';
 const settingsNextUpDays = 'filter_next_up_days';
+const settingsAutoAddFilter = 'item_auto_add_filter';
 
 const defaultSettings = {
   settingsFileTodoTxt: null,
@@ -12,6 +13,7 @@ const defaultSettings = {
   settingsThemeUseSystem: true,
   settingsThemeUseDark: false,
   settingsNextUpDays: 7,
+  settingsAutoAddFilter: true,
 };
 
 const nextUpDaysMin = 0;
