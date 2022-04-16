d12bb9c4f590d3fe46d937d117ca13b3542921f7
Ariejan de Vroom <ariejan@devroom.io>
2022-04-16 11:00:11 +0200

    Set AppDocuments path when selecting txdx directory. Closes #83

--- lib/utils/file_picker_helper.dart
+++ lib/utils/file_picker_helper.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:macos_secure_bookmarks/macos_secure_bookmarks.dart';
+import 'package:path_provider/path_provider.dart';
 
 import '../config/settings.dart';
 import '../providers/settings/settings_provider.dart';
@@ -19,9 +20,10 @@ void pickTxDxDirectory(WidgetRef ref) {
 }
 
 Future<String?> pickDir() async {
+  Directory appDocDir = await getApplicationDocumentsDirectory();
   String? result = await FilePicker.platform.getDirectoryPath(
     dialogTitle: 'Select your TxDx folder',
-
+    initialDirectory: appDocDir.path,
   );
 
   return result;
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import flutter_app_badger
 import macos_secure_bookmarks
 import package_info_plus_macos
+import path_provider_macos
 import shared_preferences_macos
 import url_launcher_macos
 import window_size
@@ -16,6 +17,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterAppBadgerPlugin.register(with: registry.registrar(forPlugin: "FlutterAppBadgerPlugin"))
   SecureBookmarksPlugin.register(with: registry.registrar(forPlugin: "SecureBookmarksPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
